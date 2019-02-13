99563066c92a270fe81b4231d3a4a740186c416f
Jose Emanuel Castelan <ingkstr@gmail.com>
2019-02-12 21:27:05 -0600

    Web_Scrapper_Completo

--- Web_Scrapper/main.py
+++ Web_Scrapper/main.py
@@ -1,6 +1,14 @@
 import argparse
 import logging
+import re
+
 """Importacion de libreria de logging a nivel INFO"""
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+import datetime
+import csv
+
 logging.basicConfig(level=logging.INFO)
 
 import news_page_objects as news
@@ -8,6 +16,8 @@ from common import config
 
 
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$') #https://example.com/hello
+is_root_path =  re.compile(r'^/.+$') #/some-text
 
 def _news_scraper(news_site_uid):
 	""""Modulo que genera el scrapper de la pagina seleccionada"""
@@ -15,8 +25,48 @@ def _news_scraper(news_site_uid):
 	logging.info('Beggining scrapper for {}'.format(host))
 	homepage = news.HomePage(news_site_uid, host)
 
+	articles = []
 	for link in homepage.articule_links:
-		print(link) 
+		#print(link)
+		article = _fetch_article(news_site_uid, host, link)
+		if article:
+			logger.info('Article fetched!')
+			articles.append(article)
+			#print(article.title)
+	#print(len(articles))
+	_save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+	now = datetime.datetime.now().strftime('%Y_%m_%d')
+	out_file_name = '{}_{}_articles.csv'.format(news_site_uid,now)
+	csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+	with open(out_file_name, mode='w+') as f:
+		writer =  csv.writer(f)
+		writer.writerow(csv_headers)
+		for article in articles:
+			row = [str(getattr(article, prop)) for prop in csv_headers]
+			writer.writerow(row)
+
+
+def  _fetch_article(new_site_uid, host, link):
+	logging.info('Start fetching article at {}'.format(link))
+	article = None
+	try:
+		article = news.ArticlePage(new_site_uid, _build_link(host, link))
+	except (HTTPError, MaxRetryError) as e:
+		logging.warning('Error while feching the article', exc_info = False)
+	if article and not article.body:
+		logging.warning('Invalid article. There is not body.')
+		return None
+	return article
+
+def _build_link(host, link):
+	if is_well_formed_link.match(link):
+		return link
+	elif is_root_path.match(link):
+		return '{}{}'.format(host, link)
+	else:
+		return '{}/{}'.format(host,link)
 
 if __name__ == '__main__':
 	"""Funcion principal"""
--- Web_Scrapper/news_page_objects.py
+++ Web_Scrapper/news_page_objects.py
@@ -1,25 +1,20 @@
 import bs4
 import requests
+
 from common import config
 
-class HomePage:
-	"""Clase del objeto HTML consultado y analizado"""
+class NewsPage:
+	"""Clase de una página de una fuente general de noticias"""
+
 	def __init__(self, news_site_uid, url):
-		"""Constructor donde se consulta a la URL"""
+		"""Constructor donde se consulta a la URL de una noticia"""
 		self._config = config()['news_sites'][news_site_uid]
 		self._queries = self._config['queries']
-		self._html = None	
+		self._html = None
 		self._visit(url)
 
-	@property
-	def articule_links(self):
-		link_list = []
-		for link in self._select(self._queries['homepage_articule_links']):
-			if link and link.has_attr('href'):
-				link_list.append(link)
-		return set(link['href'] for link in link_list)
-
 	def _select(self, query_string):
+		"""Función privada para obtener el contenido deseado con el select"""
 		return self._html.select(query_string)
 
 	def _visit(self, url):
@@ -27,3 +22,35 @@ class HomePage:
 		response = requests.get(url)
 		response.raise_for_status()
 		self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+	"""Clase del análisis de la página inicial."""
+	def __init__(self, news_site_uid, url):
+		"""Constructor"""
+		super().__init__(news_site_uid, url)
+
+	@property
+	def articule_links(self):
+		"""Obtención de los artículos de la página principal"""
+		link_list = []
+		for link in self._select(self._queries['homepage_articule_links']):
+			if link and link.has_attr('href'):
+				link_list.append(link)
+		return set(link['href'] for link in link_list)
+
+class ArticlePage(NewsPage):
+	"""Clase del análisis de una URL de noticia"""
+	def __init__(self, news_site_uid, url):
+		"""Constructor"""
+		super().__init__(news_site_uid, url)
+
+	@property
+	def body(self):
+		result = self._select(self._queries['article_body'])
+		return result[0].text if len(result) else ''
+
+	@property
+	def title(self):
+		result = self._select(self._queries['article_title'])
+		return result[0].text if len(result) else ''
